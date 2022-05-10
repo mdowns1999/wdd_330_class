081ec036e53b570c8effa89c3d6ce534437f25c4
mdowns <michaeljdowns99@gmail.com>
2022-05-10 15:19:20 -0600

    Did Reading for Week 5

--- week5/js/week5.js
+++ week5/js/week5.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function footer_date(){
     let date = new Date()
     let year = date.getFullYear()
@@ -7,6 +9,118 @@ function footer_date(){
 
 function main(){
     footer_date();
+
+    //Will get a reference Error
+    // unicorn();
+
+    //Stack Trace
+    // function three(){ unicorn(); }
+    // function two(){ three(); }
+    // function one(){ two(); }
+    // one();
+
+    //Seeing Strict Mode in action.  If the 'Use Strict' was not at the top, this will not be flagged
+    // e = 2.718;
+
+    //You could just use strict mode in one Function like this:
+    function strictly(){
+        'use strict';
+        console.log('Hello World');
+    }
+
+    //OR you could wrap all of you code in a strict function like:
+    
+    (function() {
+        'use strict';
+    
+        // All your code would go inside this function
+        console.log('Hello World');
+    
+    }());
+
+//Alerts can be used as Breakpoints to debug code.
+    // function amIOldEnough(age){
+    //     if (age = 12) {
+    //         alert(age);
+    //         return 'No, sorry.';
+    //     } else if (age < 18) {
+    //         return 'Only if you are accompanied by an adult.';
+    //     }
+    //     else {
+    //         return 'Yep, come on in!';
+    //     }
+    // }
+
+    //CONSOLE.LOG statements can be used instead of alerts as breakpoints.
+    // function amIOldEnough(age){
+    //     console.log(age);
+    //         if (age < 12) {
+    //         console.log(`In the if with ${age}`);
+    //         return 'No, sorry.';
+    //         } else if (age < 18) {
+    //         console.log(`In the else-if with ${age}`);
+    //         return 'Only if you are accompanied by an adult.';
+    //         } else {
+    //         console.log(`In the else with ${age}`);
+    //         return 'Yep, come on in!';
+    //     }
+    // }
+
+    amIOldEnough(21);
+    //If we look at the alerts, we willnotice the age = 12 is the bug.  We need the ===
+
+
+//The Debugger feature automatically runs the debugger.  
+    function amIOldEnough(age){
+        debugger;
+            if (age < 12) {
+            debugger;
+            return 'No, sorry.';
+            } else if (age < 18) {
+            debugger;
+            return 'Only if you are accompanied by an adult.';
+            } else {
+            debugger;
+            return 'Yep, come on in!';
+        }
+    }
+    
+    amIOldEnough(16);
+
+    //Will throw an error message for user
+    const error = new Error('Oops, something went wrong');
+
+    //Make a custom Error
+    const error1 = new TypeError('You need to use numbers in this function');
+
+    ////////////////////////TRY CATCH CASES//////////////////////
+    function squareRoot(number) {
+        'use strict';
+        if (number < 0) {
+            throw new RangeError("You can't find the square root of negative numbers")
+        }
+        return Math.sqrt(number);
+    };
+
+
+    function imaginarySquareRoot(number) {
+        'use strict';
+        let answer;
+        try {
+            answer = String(squareRoot(number));
+        } catch(error) {
+            answer = squareRoot(-number)+"i";
+        } finally {
+            return `+ or - ${answer}`;
+        }
+    }
+
+    let result = imaginarySquareRoot(-111)
+
+    console.log(result);
+
+    //An assert Statement in JS
+    // console.assert('expression', message)
 }
 
 main();
