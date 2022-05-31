1787f053b0a12998229339304015425de754eb58
mdowns <michaeljdowns99@gmail.com>
2022-05-31 14:10:35 -0600

    Made Minor Edits

--- week7/js/exercise7.js
+++ week7/js/exercise7.js
@@ -1,35 +1,28 @@
-function exerciseOne(n)
-{
+function exerciseOne(n) {
 
-        if (n == 0)
-        {
-            return 1;
-        }
-        else
-        {
-            return n * exerciseOne(n - 1);
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * exerciseOne(n - 1);
 
-            
-            //For Testing purposes to see if function is working
-        }
+
         //For Testing purposes to see if function is working
+    }
+    //For Testing purposes to see if function is working
 }
 
 console.log(exerciseOne(5));
 
 let list = [];
-function exerciseTwo(x,y)
-{
-    if (x === y)
-    {
+
+function exerciseTwo(x, y) {
+    if (x === y) {
         return 1;
+    } else {
+        list.push(x + 1);
+        return exerciseTwo(x + 1, y);
     }
-    else
-    {
-            list.push(x + 1);
-            return exerciseTwo(x + 1, y);
-        }
 }
 
-exerciseTwo(2,9);
+exerciseTwo(2, 9);
 console.log(list);
--- week7/js/week7.js
+++ week7/js/week7.js
@@ -8,8 +8,7 @@ function footer_date() {
     document.getElementById('year').textContent = year;
 }
 
-function chapterElevenReading()
-{
+function chapterElevenReading() {
     // function square(x) {
     //     return x*x;
     // }
@@ -20,130 +19,128 @@ function chapterElevenReading()
     console.log(square.call(null, 4))
 
     //Cache
-    function square(x){
+    function square(x) {
         square.cache = square.cache || {};
         if (!square.cache[x]) {
-            square.cache[x] = x*x;
+            square.cache[x] = x * x;
         }
         return square.cache[x]
     }
 
-square(3);
+    square(3);
 
-square(-11);
+    square(-11);
 
-//Call the chahce to see an object with key value pairs
-console.log(square.cache);
+    //Call the chahce to see an object with key value pairs
+    console.log(square.cache);
 
-function party(){
-    console.log('Wow this is amazing!');
-    party = function(){
-        console.log('Been there, got the T-Shirt');
+    function party() {
+        console.log('Wow this is amazing!');
+        party = function () {
+            console.log('Been there, got the T-Shirt');
+        }
+
+    }
+    //First Message Displays
+    party()
+
+    //Second Message displays
+    party()
+
+    //An Exmaple of a recursive funciton
+    function factorial(n) {
+        if (n === 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
     }
 
-}
-//First Message Displays
-party()
+    console.log(factorial(4))
 
-//Second Message displays
-party()
+    //A more complex recuriosn loop
+    function collatz(n, sequence = [n]) {
+        if (n === 1) {
+            return `Sequence took ${sequence.length} steps. It was ${sequence}`;
+        }
 
-//An Exmaple of a recursive funciton
-function factorial(n) {
-    if (n === 0) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
-    }
-}
+        if (n % 2 === 0) {
+            n = n / 2;
+        } else {
+            n = 3 * n + 1;
+        }
 
-console.log(factorial(4))
-
-//A more complex recuriosn loop
-function collatz(n, sequence=[n]) {
-    if (n === 1){
-        return `Sequence took ${sequence.length} steps. It was ${sequence}`;
+        return collatz(n, [...sequence, n]);
     }
 
-    if (n%2 === 0) {
-        n = n/2;
-    } else { 
-        n = 3*n + 1;
+    console.log(collatz(18))
+
+    //Using Callback
+    function wait(message, callback, seconds) {
+        setTimeout(callback, seconds * 1000);
+        console.log(message);
     }
 
-    return collatz(n,[...sequence,n]);
-}
+    function selfDestruct() {
+        console.log('BOOOOM!');
+    }
 
-console.log(collatz(18))
-
-//Using Callback
-function wait(message, callback, seconds){
-    setTimeout(callback,seconds * 1000);
-    console.log(message);
-}
-
-function selfDestruct(){
-    console.log('BOOOOM!');
-}
-
-wait('This tape will self-destruct in five seconds ... ', selfDestruct, 5);
-console.log('Hmmm, should I accept this mission or not ... ?');
+    wait('This tape will self-destruct in five seconds ... ', selfDestruct, 5);
+    console.log('Hmmm, should I accept this mission or not ... ?');
 
 }
 
 
 
-function ajaxExample()
-    {
-        //Target Buttons on home page
-        const textButton = document.getElementById('number');
-        const apiButton = document.getElementById('chuck');
-        const outputDiv = document.getElementById('output');
+function ajaxExample() {
+    //Target Buttons on home page
+    const textButton = document.getElementById('number');
+    const apiButton = document.getElementById('chuck');
+    const outputDiv = document.getElementById('output');
 
-        //Assign URLS to variables
-        const textURL = 'http://numbersapi.com/random';
-        const apiURL = 'https://api.chucknorris.io/jokes/random';
+    //Assign URLS to variables
+    const textURL = 'http://numbersapi.com/random';
+    const apiURL = 'https://api.chucknorris.io/jokes/random';
 
-        //Number Button
-        textButton.addEventListener('click', () => {
-            fetch(textURL)
-            .then( response => {
+    //Number Button
+    textButton.addEventListener('click', () => {
+        fetch(textURL)
+            .then(response => {
                 outputDiv.innerHTML = 'Waiting for response...';
-            if(response.ok) {
-                return response;
-            }
+                if (response.ok) {
+                    return response;
+                }
                 throw Error(response.statusText);
             })
-            .then( response => response.text() )
-            .then( text => outputDiv.innerText = text )
-            .catch( error => console.log('There was an error:', error))
-        },false);
+            .then(response => response.text())
+            .then(text => outputDiv.innerText = text)
+            .catch(error => console.log('There was an error:', error))
+    }, false);
 
-        //Chuck Norris Button
-        apiButton.addEventListener('click', () => {
-            fetch(apiURL)
-            .then( response => {
+    //Chuck Norris Button
+    apiButton.addEventListener('click', () => {
+        fetch(apiURL)
+            .then(response => {
                 outputDiv.innerHTML = 'Waiting for response...';
-            if(response.ok) {
-                return response;
-            }
-            throw Error(response.statusText);
+                if (response.ok) {
+                    return response;
+                }
+                throw Error(response.statusText);
             })
-            .then( response => response.json() )
-            .then( data => outputDiv.innerText = data.value )
-            .catch( error => console.log('There was an error:', error))
-        },false);
-    }
+            .then(response => response.json())
+            .then(data => outputDiv.innerText = data.value)
+            .catch(error => console.log('There was an error:', error))
+    }, false);
+}
 
 
 
 
 
-function main()
-{
-footer_date()
-chapterElevenReading()
-ajaxExample()
+function main() {
+    footer_date()
+    chapterElevenReading()
+    ajaxExample()
 }
 
 main()
