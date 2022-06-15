be3d5433aba11f20654128cee9d244f84adfaab3
mdowns <michaeljdowns99@gmail.com>
2022-06-15 11:14:10 -0600

    Added Week 10

--- week10/js/week10.js
+++ week10/js/week10.js
@@ -15,3 +15,14 @@ function main()
 }
 
 main()
+
+const email = document.getElementById("mail");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an e-mail address!");
+    email.reportValidity();
+  } else {
+    email.setCustomValidity("");
+  }
+});
